8b9d5e815d1510ad9b3a6f688cb423642dd07f4b
test <boontonadee@srru.ac.th>
2018-06-27 16:26:51 +0700

    Finish 27June

--- app/src/main/java/com/example/ajboonto/srrufriend/MainActivity.java
+++ app/src/main/java/com/example/ajboonto/srrufriend/MainActivity.java
@@ -3,11 +3,21 @@ package com.example.ajboonto.srrufriend;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.ajboonto.srrufriend.fragment.Mainfragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+//        Add Fragment to Activity
+        if (savedInstanceState == null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.contentMainFragment, new Mainfragment())
+                    .commit();
+        }
+    } //Main Method
+
+}  //Main Class
