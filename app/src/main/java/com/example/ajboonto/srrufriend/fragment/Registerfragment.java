0e3e0aa26d9920d22b6ec9cb140153c69a4ecffe
test <boontonadee@srru.ac.th>
2018-06-29 11:01:17 +0700

    check up

--- app/src/main/java/com/example/ajboonto/srrufriend/fragment/Registerfragment.java
+++ app/src/main/java/com/example/ajboonto/srrufriend/fragment/Registerfragment.java
@@ -1,20 +1,32 @@
 package com.example.ajboonto.srrufriend.fragment;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.ajboonto.srrufriend.MainActivity;
 import com.example.ajboonto.srrufriend.R;
 
 public class Registerfragment extends Fragment{
+    private ImageView imageView;
+    private Uri uri;
+    private boolean aBoolean = true // ตัวแปรถูกผิด
+
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
@@ -24,16 +36,85 @@ public class Registerfragment extends Fragment{
         creatToolbar();
         // Avata controller
 
-        ImageView imageView = getView().findViewById(R.id.imvAvata);
+        imageView = getView().findViewById(R.id.imvAvata);
         imageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(Intent.ACTION_GET_CONTENT); //ส่งให้ทำงานแทน
+                intent.setType("image/*");
+                startActivityForResult(Intent.createChooser(intent, "Please App"),1);
+
             }
         });
 
 
     }   //Main Method
+    //OncreatMenu
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.itmeuplad) {
+            uploadValueToServer(); //กด Alt+Enter
+            return true;
+
+        }
+        return super.onOptionsItemSelected(item);
+    }  // Upload
+
+
+    private void uploadValueToServer() {
+        EditText nameeditText = getView().findViewById(R.id.edtName);
+        EditText userEditText = getView().findViewById(R.id.edtUser)
+        EditText passwordEditText = getView().findViewById(R.id.edtPassword);
+
+        String nameString = nameeditText.getText().toString().trim();
+        String userString = userEditText.getText().toString().trim();
+        String passwordString = passwordEditText.getText().toString().trim();
+
+        if (aBoolean) {
+            alertMessage("Non Choose Avata")
+        } else if (nameString.isEmpty() ||userString.isEmpty() || passwordString.isEmpty()) {
+            alertMessage("Pleease Fill All Blank");
+        } else {
+
+        }
+    }
+
+    private void alertMessage(String strMessage) {
+        Toast.makeText(getActivity(), strMessage, Toast.LENGTH_SHORT)//โวยวาย ข้อความ
+    }
+
+    //Creat Menu
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.menu_register, menu);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+
+        if (resultCode == getActivity().RESULT_OK) {
+
+            uri = data.getData();
+            aBoolean = false;
+
+
+            try {
+                Bitmap bitmap = BitmapFactory.decodeStream(getActivity().getContentResolver().openInputStream(uri));
+                Bitmap bitmap1 = Bitmap.createScaledBitmap(bitmap, 800, 600, true);
+
+                imageView.setImageBitmap(bitmap1);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } // if
+    }// Result
 
     private void creatToolbar() {
         Toolbar toolbar = getView().findViewById(R.id.toolbaarRegister);
@@ -49,6 +130,7 @@ public class Registerfragment extends Fragment{
                getActivity().getSupportFragmentManager().popBackStack();
             }
         });
+        setHasOptionsMenu(true); //ฉันอนุญาตให้เมนูมาอยู่บนฉันได้
     }
 
     @Nullable
